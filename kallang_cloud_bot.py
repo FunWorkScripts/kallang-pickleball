0f5d31e047986ded8643fc03e3d9ec1f0980a9fa
FunWorkScripts <lucky.shukla8890@gmail.com>
2026-02-25 15:14:32 +0800

    Update kallang_cloud_bot.py

--- kallang_cloud_bot.py
+++ kallang_cloud_bot.py
@@ -4,7 +4,7 @@ import logging
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverClick
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import smtplib
